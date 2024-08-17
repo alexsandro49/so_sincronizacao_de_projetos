a9e2a5692a7b5cf2481e4bd7f381f39169ffddbb
Alex Sandro <alexsandro49.dev@gmail.com>
2024-08-17 11:37:02 -0300

    feat: project structure

---
+++ main.py
@@ -0,0 +1,24 @@
+# from threading import Thread
+
+def sample_collection() -> None:
+    pass
+
+
+def scientific_analysis() -> None:
+    pass
+
+
+def communication_with_the_earth() -> None:
+    pass
+
+
+def navigation() -> None:
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
